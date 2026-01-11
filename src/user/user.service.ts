914e0f0593088e3683833d434f58bad78bb391b1
Luan <luanitachi@hotmail.com>
2026-01-10 22:57:17 -0300

    add bcrypt to password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { Prisma, User } from 'src/generated/prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput) {
-    return this.prisma.user.create({ data });
+    const hashPassword = await bcrypt.hash(data.password, 10);
+    return this.prisma.user.create({
+      data: { ...data, password: hashPassword },
+    });
   }
 
   async user(
