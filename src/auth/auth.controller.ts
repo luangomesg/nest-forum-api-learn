5b5e3deef3394fb72c18bf943c18fe8e4d915ddd
Luan <luanitachi@hotmail.com>
2026-02-19 15:06:04 -0300

    att cookie configs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -27,8 +27,8 @@ export class AuthController {
 
     res.cookie('access_token', access_token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       path: '/',
       maxAge: 1000 * 60 * 60 * 24, // 1 dia
     });
