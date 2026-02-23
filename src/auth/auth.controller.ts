2d825c1420ebe85bfaa4bc563373d2c6daccc666
Luan <luanitachi@hotmail.com>
2026-02-23 03:29:36 -0300

    att auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthUserDto } from './dto/authUser.dto';
+import { Response } from 'express';
+import { Res } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -8,15 +10,23 @@ export class AuthController {
 
   @Post('signin')
   @HttpCode(HttpStatus.OK)
-  async signIn(@Body() body: AuthUserDto) {
+  async signIn(
+    @Body() body: AuthUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const { access_token } = await this.authService.signIn(
       body.email,
       body.password,
     );
 
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+    });
+
     return {
       message: 'Login realizado com sucesso',
-      access_token,
     };
   }
 }
